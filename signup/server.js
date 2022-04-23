6ebf683fa15f0cd339e4f93649cd21526717e679
Devon Boen <devonjboen@gmail.com>
2022-04-22 18:16:04 -0600

    Added initial login and signup pages

---
+++ signup/server.js
@@ -0,0 +1,12 @@
+const deleteItems = document.getElementById("submit");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+
+function clearUsername() {
+username.value= "";
+}
+
+function clearPassword() {
+password.value= "";
+}
